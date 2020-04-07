0326fc7da88cd2c0199812876777acd9e6b197ba
fadipe ayobami <fadipe.ayobami@tarvostechnology.com>
2020-04-07 19:38:24 +0100

    incident api fix

--- incidents/views.py
+++ incidents/views.py
@@ -1,15 +1,13 @@
 from django.shortcuts import render
-
-# Create your views here.
-from django.shortcuts import render
 from django.http import HttpResponse
 from django.shortcuts import get_object_or_404
 
 from rest_framework import serializers,viewsets,status,generics,permissions
 from rest_framework.response import Response
 from rest_framework.views import APIView
+from rest_framework.permissions import AllowAny
 from rest_framework.decorators import action
-from util.geolocationservice import geolocationClient
+import utils.geolocationservice as geolocation
 
 from .models import Incident
 from .serializers import IncidentSerializer,IncidentPostSerializer,IncidentDetailSerializer
@@ -19,11 +17,16 @@ import logging
 
 # Get an instance of a logger
 logger = logging.getLogger(__name__)
+logger.warn("Hello")
+logger.error("Still here...")
+logger.warn("Goodbye")
 
 # Create your views here.
 
+
 class IncidentAPIView(APIView):
     """ APIView for creating Incident as a POST Request """
+    permission_classes = [AllowAny]
     serializer_class = IncidentPostSerializer
 
     def post(self, request):
@@ -32,19 +35,23 @@ class IncidentAPIView(APIView):
         
         if serializer.is_valid(raise_exception=True):
             client_ip=get_client_ip(request)
-            geo_location_response=geolocationClient.get_location(client_ip)
+            geo_location_response=geolocation.geolocationClient.get_location(client_ip)
+            print(geo_location_response)
             logger.info('geo_location_response response:{}'.format(geo_location_response))
             data =serializer.validated_data
-            Incident.object.create(place=data.get('place'),personalNumber=data.get('personalNumber'),description=data.get('description'),
-            action=data.get('action'),ip_address=client_ip,latitude=geo_location_response['latitude'],longitude=geo_location_response['longitude'],
-            country_name=geo_location_response['country_name'],country_code=geo_location_response['country_code'],city=geo_location_response['city'],
-            region=geo_location_response['region'])
-            response = {
-                            'status': '00',
-                            'message': "Incident was created successfully "
-                        }
-            logger.info('incident was created successfully')
-            return Response(response, status=status.HTTP_200_OK)
+            if geo_location_response is not None:
+                Incident.objects.create(place=data.get('place'),personal_number=data.get('personal_number'),
+                description=data.get('description'),action=data.get('action'),
+                ip_address=client_ip,latitude=geo_location_response['latitude'],
+                longitude=geo_location_response['longitude'], country_name=geo_location_response['country_name'],
+                country_code=geo_location_response['country_code'], city=geo_location_response['city'],
+                region=geo_location_response['region'])
+                response = {
+                                'status': '00',
+                                'message': "Incident was created successfully "
+                            }
+                logger.info('incident was created successfully')
+                return Response(response, status=status.HTTP_200_OK)
         return Response({'message':'An error occurred while processing yur request','error':serializer.errors,}, status=status.HTTP_400_BAD_REQUEST)
 
 
@@ -52,6 +59,7 @@ class IncidentAPIView(APIView):
 
 class IncidentRetrieveUpdateDestroy(generics.RetrieveUpdateDestroyAPIView):
     """ APIView for updating,editing and deleting Incident it accept muliple HTTP Request get,post,delete etc """
+    permission_classes = [AllowAny]
     serializer_class =IncidentDetailSerializer
     lookup_field = 'id'
     queryset = Incident.objects.all()
@@ -81,6 +89,7 @@ class IncidentRetrieveUpdateDestroy(generics.RetrieveUpdateDestroyAPIView):
 
 class IncidentList(generics.ListAPIView):
     """ APIView that returns list of  Incident a GET Request """
+    permission_classes = [AllowAny]
     serializer_class=IncidentDetailSerializer
     queryset = Incident.objects.all()
     ordering_fields = ('created_at', 'personal_number')
@@ -98,7 +107,7 @@ def get_client_ip(request):
     return ip
 
 
-class Incident(viewsets.ModelViewSet):
+class IncidentViewSet(viewsets.ModelViewSet):
     queryset = Incident.objects.all()
     serializer_class = IncidentSerializer
 
--- utils/geolocationservice.py
+++ utils/geolocationservice.py
@@ -1,6 +1,6 @@
 from django.conf import settings
 from ipdata import ipdata
-from pprint import pprint
+import requests
 # import the logging library
 import logging
 
@@ -12,14 +12,17 @@ To get user geo location service using ipaddress
 
 class geolocationClient:  
     API_KEY = '1fedcb73d9a45ffb9e0652bf68bef7ace46f8394b6d944c3351941bf'
+    BASE_URL='https://api.ipdata.co/{}?api-key={}'
     @classmethod
     def get_location(cls,ip_address):
-        logger.info('start:getting user location service using ipaddress')
+        logger.info('start:getting user location service using ipaddress:{}'.format(ip_address))
         try:
             if ip_address is not None:
-                ipdata = ipdata.IPData(self.API_KEY)
-                response = ipdata.lookup(ip_address)
-                logger.info('ipdata response response: {},response code :{}'.format(response.json(),response.status_code))
+                #ipdata = ipdata.IPData(cls.API_KEY)
+                response = requests.get('https://api.ipdata.co/{}?api-key={}'.format('188.148.194.57',cls.API_KEY))
+                #ipdata.lookup(ip_address)
+                print(response)
+                logger.info('ipdata response response data: {}'.format(response.json()))
                 return response.json()
                
         except Exception as e:
