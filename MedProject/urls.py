6d13e9ee71937029d853ac41478325529574044d
fadipe ayobami <fadipe.ayobami@tarvostechnology.com>
2020-04-21 12:10:32 +0100

    fix bug

--- MedProject/urls.py
+++ MedProject/urls.py
@@ -39,7 +39,7 @@ schema_view = get_schema_view(
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('^swagger(?P<format>\.json|\.yaml)$', schema_view.without_ui(cache_timeout=0), name='schema-json'),
+    #path('^swagger(?P<format>\.json|\.yaml)$', schema_view.without_ui(cache_timeout=0), name='schema-json'),
     path('swagger', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
     path('openapi/', TemplateView.as_view(template_name="swagger-ui/dist/index.html")),
     path('auth/',include('appAuths.urls')),
